af0fe815cdddb4bf90dcde3e1723188cd241986c
Sebastian Hereñu Amaral <56357599+SHA-Arg@users.noreply.github.com>
2024-05-30 23:36:39 -0300

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,62 +1,24 @@
-import pygame
-import numpy as np
-import time
+import numpy as np 
 
+N = 10
+ON = 1
+OFF = 0
 
-pygame.init()
+grid = np.full((N,N), OFF)
 
-width , heigth = 1000 , 1000
-screen = pygame.display.set_mode((heigth , width))
-bg = 25, 25, 25
-screen.fill(bg)
-
-nxC, nyC = 25, 25
-dimCW = width  / nxC
-dimCH = heigth / nyC
-
-Alive = 1; Dead = 0;
-gameState = np.zeros((nxC, nyC))
-
-# Automata #2.
-gameState[21, 21] = 1
-gameState[22, 22] = 1
-gameState[22, 23] = 1
-gameState[21, 23] = 1
-gameState[20, 23] = 1
-
-pauseExect = False
-
-while True:
-    newGameState = np.copy(gameState)
-    screen.fill(bg)
-    time.sleep(0.1)
-    ev = pygame.event.get()
-    for event in ev:
-        if event.type == pygame.KEYDOWN:
-            pauseExect = not pauseExect
-
-    for y in range(0, nxC):
-        for x in range(0, nyC):
-            if not pauseExect:
-                n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
-                      gameState[(x) % nxC, (y - 1) % nyC] + \
-                      gameState[(x + 1) % nxC, (y - 1) % nyC] + \
-                      gameState[(x - 1) % nxC, (y) % nyC] + \
-                      gameState[(x + 1) % nxC, (y) % nyC] + \
-                      gameState[(x - 1) % nxC, (y + 1) % nyC] + \
-                      gameState[(x) % nxC, (y + 1) % nyC] + \
-                      gameState[(x + 1) % nxC, (y + 1) % nyC] 
-            if gameState[x, y] == 0 and n_neigh == 3:
-                newGameState[x, y] = 1
-            elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh >3):
-                newGameState[x, y] = 0               
-            poly = [((x) * dimCW, y * dimCH),
-                    ((x+1) * dimCW, y * dimCH),
-                    ((x+1) * dimCW, (y+1) * dimCH),
-                    ((x) * dimCW, (y+1) * dimCH)]
-            if newGameState[x, y] == 0:
-                pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+def update(grid):
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+                    grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                    grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
+                    grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N]))
+            if grid[i,j] == ON:
+                if (total<2) or (total>3):
+                    newGrid[i,j] = OFF
             else:
-                pygame.draw.polygon(screen, (255, 255, 255), poly, 0)
-gameState = np.copy(newGameState)   
-pygame.display.flip()
+                if total == 3:
+                    newGrid[i,j] = ON
+                
+    return newGrid
