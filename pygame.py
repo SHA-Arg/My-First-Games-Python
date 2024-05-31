22e807056bd73c4a973a6c0346df36e5b6308fe1
Sebastian Hereñu Amaral <56357599+SHA-Arg@users.noreply.github.com>
2024-05-30 23:37:18 -0300

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -1,47 +1,84 @@
-import pygame
-
+import tkinter as tk 
 import numpy as np
+from game_of_life import update, grid, N, ON, OFF
 
+root = tk.Tk()
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.resizable(0,0)
 
+def on_click(i, j, grid, buttons):
+    grid[i,j] = 1 if grid[i,j] == 0 else 0
+    buttons[i][j].config(bg='black' if grid[i,j] else 'white')
+    
+buttons= []
+for i in range(N):
+    row = []
+    for j in range(N):
+        button = tk.Button(
+            root,
+            command = lambda i=i, j=j: on_click(i, j, grid, buttons),
+            height = 2,
+            width = 4,
+            bg = 'black' if grid[i,j] else 'white'
+        )
+        row.append(button)
+    buttons.append(row)
+    
+for i in range(N):
+    for j in range(N):
+        buttons[i][j].grid(row=i, column=j, sticky="nsew")
+        
+def gui_update():
+    global grid
+    grid = update(grid)
+    for i in range(N):
+        for j in range(N):
+            buttons[i][j].config(bg='black' if grid[i,j] else 'white')
+    if running:
+        root.after(speed_dict[speed_var.get()], gui_update)
 
-width, height = 1000, 1000
+def start():
+    global running
+    running = True
+    gui_update()
 
-screen = pygame.display.set_mode((height, width))
+def stop():
+    global running
+    running = False
+    
+def step():
+    if not running:
+        gui_update()
+        
+def clear():
+    global grid
+    stop()
+    grid = np.full((N,N), OFF)
+    for i in range(N):
+        for j in range(N):
+            buttons[i][j].config(bg='black' if grid[i,j] else 'white')
+    
+    
 
+start_button = tk.Button(root, text='Comenzar', command= lambda: start())
+start_button.grid(row=N, column = 0, columnspan=N//5, sticky="we")
 
-bg = 25, 25, 25
+stop_button = tk.Button(root, text='Parar', command= lambda: stop())
+stop_button.grid(row=N, column = N//5, columnspan=N//5, sticky="we")
 
-screen.fill(bg)
+step_button = tk.Button(root, text='Step', command= lambda: step())
+step_button.grid(row=N, column = 2*N//5, columnspan=N//5, sticky="we")
 
+clear_button = tk.Button(root, text='Limpiar', command = lambda: clear())
+clear_button.grid(row=N, column = 3*N//5, columnspan=N//5, sticky="we")
 
-nxC, nyC = 25, 25
+speed_dict = {'Lento': 500, 'Normal': 200, 'Raido': 100, 'Rapidito' : 50, 'MaxJoputa': 10}
+speed_var = tk.StringVar(root)
+speed_var.set('Normal')
+speed_option = tk.OptionMenu(root, speed_var, *speed_dict.keys())
+speed_option.grid(row=N, column = 4*N//5, columnspan=N//5, sticky="we")
 
+running = False
 
-dimCW = width / nxC
-
-dimCH = height /  nyC
-
-
-while True:
-
-
-    for y in range(0, nxC):
-
-        for x in range(0, nyC):
-
-
-            poly = [((x) * dimCW, y * dimCH),
-
-                    ((x+1) * dimCW, y * dimCH),
-
-                    ((x+1) * dimCW, (y+1) * dimCH),
-
-                    ((x) * dimCW, (y+1)* dimCH)]
-
-
-            pygame.draw.polygon(screen, (128, 128, 128), poly ,1)        
-
-
-pygame.display.flip()
-
-
+root.mainloop()
